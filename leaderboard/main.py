9233b021767474fc7a45ca450db1185e6475de07
Nabila Khansa <nabila.khansa@gmail.com>
2024-10-23 23:59:53 +0700

    mod/s2/finals ready

--- leaderboard/main.py
+++ leaderboard/main.py
@@ -25,61 +25,15 @@ def main():
 
 {create_leaderboard(data, config["teams"], with_cp=with_cp)} 
 {create_penalty_table('- Wildcard')} 
-{add_penalty(
-    game='1',
-    team='Eclair',
-    penalty='-1',
-    reason='Non Player Death (Caffeine)'
-)}
-{add_penalty(
-    game='1',
-    team='DegaDegi',
-    penalty='-1',
-    reason='Non Player Death (Azurieru)'
-)}
-{add_penalty(
-    game='1',
-    team='HeyTayo',
-    penalty='-1',
-    reason='Non Player Death (Onryou)'
-)}
-{add_penalty(
-    game='1',
-    team='HeyTayo',
-    penalty='-1',
-    reason='Non Player Death (ZxLaim)'
-)}
-{add_penalty(
-    game='2',
-    team='ViciVeni',
-    penalty='-1',
-    reason='Non Player Death (Iz1Senpai)'
-)}
-{add_penalty(
-    game='2',
-    team='CEPU',
-    penalty='-1',
-    reason='Non Player Death (CEPU-Valsh)'
-)}
-{add_penalty(
-    game='4',
-    team='StepuhSons',
-    penalty='-1',
-    reason='Non Player Death (Saiikyouu)'
-)}
-{add_penalty(
-    game='4',
-    team='StepuhSons',
-    penalty='-1',
-    reason='Non Player Death (Oshunicus)'
-)}
-{add_penalty(
-    game='5',
-    team='LastTeam',
-    penalty='-1',
-    reason='Non Player Death (Gallileo)'
-)}
-"""
+{add_penalty()}
+""" 
+# add_penalty() /no param/ itu untuk filler row ketika belum ada penalty
+# {add_penalty(
+#     game='x',
+#     team='ipsum',
+#     penalty='-1',
+#     reason='Non Player Death (player)'
+# )}
 
     page.set_header()
     page.add_content(leaderboard)
